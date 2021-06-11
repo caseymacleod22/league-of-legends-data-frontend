1b8ed6a78e3a1b35ad02c498ce914d3ddb4bf937
Casey MacLeod <caseymacleod22@gmail.com>
2021-06-11 14:37:09 -0400

    wip: log champion names, champions component working properly

--- src/components/Champions/Champions.js
+++ src/components/Champions/Champions.js
@@ -2,6 +2,11 @@ const Champions = (props) => {
     for (const [key, value] of Object.entries(props.data)) {
         console.log(`${key}: ${value}}`)
     }
+    return (
+        <div>
+            TEST
+        </div>
+    )
 }
 
 export default Champions
