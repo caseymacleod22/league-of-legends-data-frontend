f19f7cb31fabe30b564918392c3bd05c3571ac29
Casey MacLeod <caseymacleod22@gmail.com>
2021-06-13 23:00:49 -0400

    wip: champion title function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState} from 'react'
 import './App.css';
 import { getChampionData } from './services/league-api';
 import Champions from './components/Champions/Champions'
+import Title from './components/Title/Title'
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
       <h1>League of Legends</h1>
       <div>
         <Champions data={appData}/>
+        <Title data={appData} />
       </div>
     </div>
   )
---
+++ src/components/Title/Title.js
@@ -0,0 +1,18 @@
+const Title = (props) => {
+    const championTitle = Object.keys(props.data)
+    console.log(championTitle)
+    
+    return (
+        <div>
+            {championTitle.map((champion) => {
+                return (
+                    <div>
+                        {champion}
+                    </div>
+                )
+            })}
+        </div>
+    )
+    }
+    
+    export default Title
