2a5f218bb85ac827a9528c72bf2691778ea87e8a
Casey MacLeod <caseymacleod22@gmail.com>
2021-06-14 15:50:12 -0400

    wip :champion component refactor console log

--- src/components/Champions/Champions.js
+++ src/components/Champions/Champions.js
@@ -3,12 +3,14 @@ const championName = []
 for (let i in props.data) {
     championName.push([i, props.data [i]])
 }
-const champion = championName[75][1]['name']
-console.log(championName[75][1]['name'])
 
 return (
     <div>
-        {champion}
+        {championName.map((champion) => {
+            return (
+                <div>{champion.name}</div>
+            )
+        })}
     </div>
 )
 }
